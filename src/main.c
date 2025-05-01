8d06b99b7c3b318117f72557df5058e82928b14d
Maximilian Fichtl <mfichtl@brasfanta.com.br>
2025-05-01 18:58:44 -0300

    program has height parameter

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "hanoi.h"
 
-int main(void) {
-    hanoi(3,1,3);
+int main(int argc, char *argv[]) {
+    int height = 3;
+    if (argc <= 2) {
+        if (argc == 2)
+            height = atoi(argv[1]);
+    }
+    else {
+        fprintf(stderr, "Usage: hanoi [height] (with 0 < height < 9\n)");
+        return(-1);
+    }
+    if (height < 1 || height > 8) {
+        fprintf(stderr, "Required 0 < height < 9\n");
+        return(-2);
+    }
+    hanoi(height, 1, 3);
+    return(0);
 }
